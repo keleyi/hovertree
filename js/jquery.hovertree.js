7f1fbfdc6a3ad5d87e8038995010cdcb45ba88f2
keleyi <le@mail.keleyi.com>
2014-09-30 02:45:20 +0800

    hovertree 0.1.0
    hovertree 0.1.0  Tree Menu

---
+++ js/jquery.hovertree.js
@@ -0,0 +1,37 @@
+﻿/*!
+* HoverTree(jQuery Menu)
+* version: 0.1.0
+* Copyright (c) 2014 KeLeyi
+* http://keleyi.com
+* http://keleyi.com/jq/hovertree/
+*/
+
+(function ($) {
+    $.fn.hovertree = function (options) {
+
+        var settings = $.extend({
+            width: '',
+            isCloseOther: false//当要展开一个一级菜单时，关闭其他已经展开的一级菜单项。
+        }, options);
+
+        var keleyihovertree = $(this);
+
+        if (settings.width != '') {
+            keleyihovertree.css({ "width": settings.width });
+        }
+
+        keleyihovertree.find("h3").on("click", function () {
+            var headParent = $(this).parent();
+            
+            if (headParent.attr("class").indexOf("current") > 0) {
+                headParent.removeClass("current")
+            } else {
+                if (settings.isCloseOther) {
+                    keleyihovertree.find(">div.current").removeClass("current");
+                }
+                headParent.addClass("current")
+            }
+        })
+
+    }
+} (jQuery));
